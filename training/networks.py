a430a0d933fa9a4881635f0c5023a6e5ef76a360
doroghazi <doroghazi@gmail.com>
2023-01-22 15:35:49 -0500

    Update networks.py
    Changed tf.get_variable to tf.compat.v1.get_variable for compatibility with tf 2.x

--- training/networks.py
+++ training/networks.py
@@ -385,7 +385,7 @@ def G_synthesis(
         for layer_idx in range(num_layers - 1):
             res = (layer_idx + 5) // 2
             shape = [1, 1, 2**res, 2**res]
-            noise_inputs.append(tf.get_variable(f'noise{layer_idx}', shape=shape, initializer=tf.initializers.random_normal(), trainable=False))
+            noise_inputs.append(tf.compat.v1.get_variable(f'noise{layer_idx}', shape=shape, initializer=tf.initializers.random_normal(), trainable=False))
 
     # Single convolution layer with all the bells and whistles.
     def layer(x, layer_idx, fmaps, kernel, up=False):
